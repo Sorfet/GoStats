b1e9949a5eddccf3c809fd57c172f2dd0d394503
Sorfet <sorfetti@gmail.com>
2015-02-15 16:10:13 +0200

    Lisätty Main: - placeholder tausta - tekstikenttä (idInput) - nappi (etuNappi)
    Java:
    - koodiin todo listaa
    
    v0.1.1

--- app/src/main/java/com/sasumikonranta/gostats/Main.java
+++ app/src/main/java/com/sasumikonranta/gostats/Main.java
@@ -6,12 +6,64 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
+/* Comments::
+    TODO:
+
+- Parseeja
+  - Data saadaan tästä: http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=<<KEY>>&steamid=<<PROFILEID>>
+
+  <<key>> = 7B99A8C2129627CC394A4B3FD17F0A12
+
+  ja <<profile id>> vaik sorfet:  76561197990716071
+
+
+  eli tässä:   http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=7B99A8C2129627CC394A4B3FD17F0A12&steamid=76561197990716071
+
+Arvoja kaikilla muuttujilla on:
+  name
+  value
+
+  "playerstats": {
+		"steamID": "76561197990716071",
+		"gameName": "ValveTestApp260",
+		"stats": [
+			{
+				"name": "total_kills",
+				"value": 9836
+			},
+			{
+				"name": "total_deaths",
+				"value": 11507
+			}]
+
+			//Tästä leikattu ja suljettu.. näkee vain rakenteen
+
+			}
+}
+
+
+- layout
+
+
+
+
+*/
+
 public class Main extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
     }
 
 
